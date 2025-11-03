d74fd28eb8eedb06656651d31176103dc828b31d
Vishal Panchal <vishal.panchal@bypt.in>
2025-11-03 15:21:55 +0530

    Committed the latest source code

--- src/app/nft/page.jsx
+++ src/app/nft/page.jsx
@@ -12,7 +12,7 @@ export default function Home() {
     setTxHash("");
 
     try {
-      const res = await fetch("http://localhost:5000/mint", {
+      const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/mint`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
